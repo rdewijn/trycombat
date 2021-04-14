aa6abdcdabbef6998215863887c7b95ce3054562
rdewijn <36480607+rdewijn@users.noreply.github.com>
2021-04-14 18:28:33 +0200

    ++

--- setup_test.r
+++ setup_test.r
@@ -10,58 +10,71 @@ if( !("pgBatch" %in% installed.packages())) renv::restore()
 
 library(pgBatch)
 
+# helper to create input matrix for combat
 input.mat = function(df, vcol = "H_Identity", bcol = "RunID"){
   X = acast(df, ID ~ Barcode + Array, value.var = vcol)
-  bx = acast(df, ID ~ Barcode + Array, value.var = bcol)
+  bx = acast(df, ID ~ Barcode + Array, value.var = bcol)[1,]
   result = list(X = X, bx = factor(bx))
 }
 
 # edit path to load data
-h = read.csv("C:/PamSoft/PamCloud/jcrLocalRoot/PamCloud/Biolizard/CC_CPE/cc_cpe_normalized.txt")
+h = read.csv("C:/PamSoft/PamCloud/jcrLocalRoot/PamCloud/Biolizard/CC_CPE/cc_cpe_normalized.txt") %>%
+  filter( !(Barcode == "650055110" & Array == "A2"))
 f = read.csv("C:/PamSoft/PamCloud/jcrLocalRoot/PamCloud/Biolizard/CC_CPE/cc_cpe_corrected.txt")
 
-hcc.ref = h %>%
-  filter(Study == "METHOD-CC") %>%
-  filter(Grouping == "REF")
-
-hcc.das = h %>%
-  filter(Study == "METHOD-CC") %>%
-  filter(Grouping == "DAS")
-
 # fit a model for run-run in the cc set on the REF data
-#Xref = acast(hcc.ref, ID ~ Barcode + Array, value.var = "H_Identity")
-#bx.ref = acast(hcc.ref, ID ~ Barcode + Array, value.var = "RunID")[1,]
+cc.ref = h %>%
+  filter(Study == "METHOD-CC") %>%
+  filter(Grouping == "REF") %>%
+  input.mat()
 
-cc.ref = input.mat(hcc.ref)
 cc.model  = pgCombat$new()
 cc.model = cc.model$fit(cc.ref$X, cc.ref$bx, mean.only = TRUE)
 
 # Apply to the DAS set
-Xcc = acast(hcc.das, ID ~ Barcode + Array, value.var = "H_Identity")
-bx = acast(hcc.das, ID ~ Barcode + Array, value.var = "RunID")[1,]
-Xcc.corrected = cc.model$apply(Xcc, factor(bx))
+cc.das = h %>%
+  filter(Study == "METHOD-CC") %>%
+  filter(Grouping == "DAS") %>%
+  input.mat()
 
-# model correction of the CPE set towards the CC set
-hcpe.ref = h %>%
+Xcc.corrected = cc.model$apply(cc.das$X, cc.das$bx)
+
+# CPE towards CC
+cpe.ref = h %>%
   filter(Study == "METHOD-CPE") %>%
-  filter(Grouping == "REF")
+  filter(Grouping == "REF") %>%
+  input.mat()
 
-hcpe.das = h %>%
-  filter(Study == "METHOD-CPE") %>%
-  filter(Grouping == "DAS")
-
-Xcpe.ref = acast(hcpe.ref, ID ~ Barcode + Array, value.var = "H_Identity")
-bx.cpe = acast(hcpe.ref, ID ~ Barcode + Array, value.var = "RunID")[1,]
-
-M = cbind(cc.model$Xc, Xcpe.ref)
-bxm = c(rep("cc-ref", dim(cc.model$Xc)[2]), bx.cpe)
+R = cc.model$apply(cc.ref$X, cc.ref$bx) # ref data for CPE
+br = rep("cc-ref", dim(R)[2])
+M = cbind(R, cpe.ref$X)
+bxm = c(br, as.character(cpe.ref$bx)) %>% as.factor()
 
 cpe.model = pgCombat$new()
-cpe.model = cpe.model$fit(M, factor(bxm), mean.only = TRUE, ref.batch = "cc-ref")
+cpe.model = cpe.model$fit(M, bxm, mean.only = TRUE, ref = "cc-ref")
 
-Xcpe.das = acast(hcpe.das, ID ~ Barcode + Array, value.var = "H_Identity")
-bx = acast(hcpe.das, ID ~ Barcode + Array, value.var = "RunID")[1,]
+cpe.das = h %>%
+  filter(Study == "METHOD-CPE") %>%
+  filter(Grouping == "DAS") %>%
+  input.mat()
 
-Xcpe.corrected = cpe.model$apply(Xcpe.das, factor(bx) )
+Xcpe.corrected = cpe.model$apply(cpe.das$X, cpe.das$bx)
+
+# check workflow
+# Note: some difference between the exported result and the result created here ...
+
+cpe.corrected = f %>%
+  filter(Study == "METHOD-CPE") %>%
+  filter(Grouping == "DAS") %>%
+  input.mat(vcol =   "CPECCCor_Identity")
+
+MC = cpe.corrected$X
+delta = Xcpe.corrected - MC
+hist(delta)
+
+
+
+
+
 
 
